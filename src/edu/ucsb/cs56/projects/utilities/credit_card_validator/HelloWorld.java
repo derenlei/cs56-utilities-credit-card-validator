099f2dc660e21d0375bb2e91eef3cc93b144dc94
Deren Lei <derenlei@umail.ucsb.edu>
2017-11-08 00:24:33 -0800

    Add HelloWorld

---
+++ src/edu/ucsb/cs56/projects/utilities/credit_card_validator/HelloWorld.java
@@ -0,0 +1,13 @@
+package edu.ucsb.cs56.projects.utilities.credit_card_validator;
+import static spark.Spark.*;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+	get("/hello", new Route() {
+		@Override
+		public Object handle(Request request, Response response) {
+		    return "Hello World";
+		}
+	    });
+    }
+}
